04b6a0fba5c54e97264ac093919fb1a855bbfd77
Amireddy Prashanth Reddy <amireddyprashanth19@gmail.com>
2020-07-16 22:41:30 +0530

    Completely removed the RoadBike and MountainBike subclasses, and introduced Part class (in addition to Parts) Greatly demonstrates the power of composition over inheritence: When a new Bike subclass is to be implemented, we can simply construct the corresponding Parts and accumulate them to form it Note: Some default initializations and overloaded constructed are taken away, as the main objective is to understand Composition phenomenon ToDo: An array is being used as the collection to store Parts. This may not be possible if different parts have different properties (like suppose a part has two descriptions, how would you implement it?)

--- BicycleDemo.java
+++ BicycleDemo.java
@@ -15,118 +15,52 @@ class Bicycle{
 	}
 }
 
-abstract class Parts{
-	protected String chain;
-	protected String tireSize;
+class Parts{
+	private Part[] parts;
 
-	public Parts(String chain, String tireSize){
-		this.chain = chain;
-		this.tireSize = tireSize;
+	public Parts(Part[] parts){
+		this.parts = parts;
 	}
-
-	abstract public HashMap<String, String> localSpares();
-
-	public String defaultChain(){
-		if(chain!=null) return chain;
-		return "10-speed";
-	}
-
-	abstract public String defaultTireSize();
 
 	public HashMap<String, String> getSpares(){
 		HashMap<String, String> newMap = new HashMap<> ();
-		newMap.put("chain", defaultChain());
-		newMap.put("tireSize", defaultTireSize());
-		newMap.putAll(localSpares());
+		for(Part pt: parts){
+			if(pt.needsSpare) newMap.put(pt.name, pt.description);
+		}
 		return newMap;
 	}
-
-	// declare methods
 }
 
-class RoadBikeParts extends Parts{
-	public String tapeColor;
+class Part{
+	public String name;
+	public String description;
+	public boolean needsSpare;
 
-	public RoadBikeParts(String chain, String tireSize, String tapeColor){
-		super(chain, tireSize);
-		this.tapeColor = tapeColor;
+	public Part(String name, String description, boolean needsSpare){
+		this.name = name;
+		this.description= description;
+		this.needsSpare = needsSpare;
 	}
-
-	public HashMap<String, String> localSpares(){
-		HashMap<String, String> newMap = new HashMap<String, String> ();
-		newMap.put("tapeColor", tapeColor);
-		return newMap;
-	}
-
-	public String defaultTireSize(){
-		if(tireSize!=null) return tireSize;
-		return "23";
-	}
-
 }
-
-class MountainBikeParts extends Parts{
-	private String frontShock;
-	private String rearShock;
-
-	public MountainBikeParts(String chain, String tireSize, String frontShock, String rearShock){
-		super(chain, tireSize);
-		this.frontShock = frontShock;
-		this.rearShock = rearShock;
-	}
-	
-	public HashMap<String, String> localSpares(){
-		HashMap<String, String> newMap = new HashMap<String, String> ();
-		newMap.put("rearShock", rearShock);		
-		return newMap;
-	}
-
-	public String defaultTireSize(){
-		if(tireSize!=null) return tireSize;
-		return "2.1";
-	}
-
-}
-
-// class RecumbentBike extends Bicycle{
-// 	private String flag;
-// 	public RecumbentBike(String size, String chain, String tireSize, String frontShock, String rearShock, String flag){
-// 		super(size, chain, tireSize);
-// 		this.flag = flag;
-// 	}
-
-// 	public HashMap<String, String> localSpares(){
-// 		HashMap<String, String> newMap = new HashMap<String, String> ();
-// 		newMap.put("flag", flag);		
-// 		return newMap;
-// 	}
-
-// 	public String defaultChain(){
-// 		if(chain!=null) return chain;
-// 		return "9-speed";
-// 	}
-
-// 	public String defaultTireSize(){
-// 		if(tireSize!=null) return tireSize;
-// 		return "28";
-// 	}
-
-// }
 
 public class BicycleDemo{
 	public static void main(String[] args){
-		RoadBikeParts parts = new RoadBikeParts("30-speed", "100", "red");
-		Bicycle bike = new Bicycle("M", parts);
+		Part chain = new Part("chain", "10-speed", true);
+		Part roadTire = new Part("tireSize", "23", true);
+		Part tape = new Part("tapeColor", "red", true);
+		Part mountainTire = new Part("tireSize", "2.1", true);
+		Part rearShock = new Part("rearShock", "Fox", true);
+		Part frontShock = new Part("frontShock", "Manitou", false);
+
+		Parts roadBikeParts = new Parts(new Part[]{chain, roadTire, tape});
+		Bicycle bike = new Bicycle("L", roadBikeParts);
 		System.out.println(bike.size);
 		System.out.println(bike.getSpares());
 
-		MountainBikeParts parts1 = new MountainBikeParts(null, null, "Manitou", "Fox");
-		Bicycle bike1 = new Bicycle("S", parts1);
-		System.out.println(bike1.size);
-		System.out.println(bike1.getSpares());
+		Parts mountainBikeParts = new Parts(new Part[]{chain, mountainTire, frontShock, rearShock});
+		Bicycle bike2 = new Bicycle("M", mountainBikeParts);
+		System.out.println(bike2.size);
+		System.out.println(bike2.getSpares());
 
-		// RecumbentBike bike2 = new RecumbentBike("L", null, null, null, null, "orange");
-		// System.out.println(bike2.size);
-		// System.out.println(bike2.getSpares());
 	}
 }
